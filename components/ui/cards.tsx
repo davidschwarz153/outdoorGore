2ad5ec3e91ddc4a367aa28af9576e40d291a7455
davidschwarz153 <schwarz.david100@gmail.com>
2025-02-03 11:08:25 +0100

    added keys

--- components/ui/cards.tsx
+++ components/ui/cards.tsx
@@ -11,26 +11,32 @@ type Product = {
   }
 }
 
-export default async function Cards(){
-    const cards = await fetch("https://fakestoreapi.com/products")
-    const data = await cards.json()
+export default async function Cards() {
+  const cards = await fetch("https://fakestoreapi.com/products");
+  const data = await cards.json();
 
-    return (
-      <div className="md:grid md:grid-cols-4 md:gap-10">
-        {data.map((el: Product )=> {
-            return (
-              <article className="text-emerald-950 h-[40vh] flex flex-col justify-center p-10 border-2 rounded-lg shadow-sm cursor-pointer transform transition-transform duration-200 hover:scale-105" >
-                  <img src={el.image} className="h-60 w-auto object-contain mb-2" alt={el.title + ' ' + 'img'} />
-                  <h1 className="truncate">{el.title}</h1>
-                  <p className="truncate mb-6">{el.description}</p>
-                  <div className="flex justify-between">
-                    <p>{el.rating.rate + '/5'}</p>
-                    <p>{el.price}</p>
-                  </div>
-
-              </article>
-            )
-        })}
-      </div>
-    )
+  return (
+    <div className="md:grid md:grid-cols-4 md:gap-10">
+      {data.map((el: Product) => {
+        return (
+          <article
+            key={el.id} // Added key prop here
+            className="text-emerald-950 h-[40vh] flex flex-col justify-center p-10 border-2 rounded-lg shadow-sm cursor-pointer transform transition-transform duration-200 hover:scale-105"
+          >
+            <img
+              src={el.image}
+              className="h-60 w-auto object-contain mb-2"
+              alt={el.title + " img"}
+            />
+            <h1 className="truncate">{el.title}</h1>
+            <p className="truncate mb-6">{el.description}</p>
+            <div className="flex justify-between">
+              <p>{el.rating.rate + "/5"}</p>
+              <p>{el.price}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 }
--- components/ui/catergories.tsx
+++ components/ui/catergories.tsx
@@ -1,13 +1,20 @@
-export default async function Categories(){
-  const data = await fetch("https://fakestoreapi.com/products/categories")
-  const cat = await data.json()
-    
+export default async function Categories() {
+  const data = await fetch("https://fakestoreapi.com/products/categories");
+  const cat = await data.json();
 
   return (
     <div className="flex justify-center items-center gap-20 mt-5 mb-20">
       {cat.map((el: string) => {
-        return <div className="border-2 py-2 px-4 bg-emerald-700 rounded-xl shadow-xl text-sm cursor-pointer transform transition-transform duration-200 hover:scale-105" id={el}>{el.toUpperCase()}</div>
+        return (
+          <div
+            key={el} 
+            className="border-2 py-2 px-4 bg-emerald-700 rounded-xl shadow-xl text-sm cursor-pointer transform transition-transform duration-200 hover:scale-105"
+            id={el}
+          >
+            {el.toUpperCase()}
+          </div>
+        );
       })}
-    </div>   
-  )   
+    </div>
+  );
 }
